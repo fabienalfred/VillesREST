308cdc24ec469ebc2eb6821eb3486a3f7530e481
Fabien Alfred <alfred.fabien@gmail.com>
2015-08-14 16:53:33 +0200

    n commit

--- WebContent/js/villes.js
+++ WebContent/js/villes.js
@@ -1,12 +1,14 @@
 getVilles = function(cp, callbackFunction){
 	var url = "rest/villes/cp/"+cp;
-	console.log("ok");
+	console.log("ok3");
 	$.getJSON(url, function(data){
+		console.log("ok4");
 		callbackFunction(data);
 	});
 };
 
 function showVilles(data){
+	console.log("ok5");
 	var html = "";
 	$.each(data, function(index, ville){
 		html += "<option value='" + ville.id
@@ -21,7 +23,9 @@ function showVilles(data){
 }
 
 $(document).ready(function(){
+	console.log("ok1");
 	$("#button").click(function(){
+		console.log("ok2");
 		getVilles($("#cp").val(), showVilles);
 	});
 })
